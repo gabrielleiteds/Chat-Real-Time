510322a6c162463be76cda273ec09ccd4bf78de3
Gabriel <leitedasilvag@gmail.com>
2020-05-16 15:50:04 -0300

    getting started

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express(); 
+
+//server
+const server = require('http').createServer(app); 
+//config protocol WSS(web socket)
+const io = require('socket.io')(server); 
+
+//configuration for archives css, js, images and files .html(views) 
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.set('views', path.join(__dirname, 'public'));
+
+//config archives using .html with ejs  
+app.engine('html', require('ejs').renderFile); 
+app.set('view engine', 'html'); 
+
+app.use('/', (req, res) => {
+    res.render('index.html'); 
+})
+
+server.listen(3000); 
